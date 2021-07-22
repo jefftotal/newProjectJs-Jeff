073342f8c87fd9b2748a736e67269c9999da3d7c
Jeff JS. Santos <jeff@lupus.intranet>
2021-07-21 21:54:50 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@
 function Home(){
     return (
         <div>
-            <img src="https://www.economart.com.br/wp-content/uploads/2020/03/Logo_Economart_2020.png"/>
+            <img src="https://www.economart.com.br/wp-content/uploads/2020/03/Logo_Economart_2020.png" sizes />
             <h2>Página para solicitação de Pré-Venda Consinco do ifood</h2>
+            <button>Solicitar Pré-Venda</button>
         </div>
 
     )
